5f88b1db870a16aabe26e4e8da66d298865b1728
Varocraft25 <125608187+Varocraft25@users.noreply.github.com>
2024-05-24 10:23:02 +0100

    Nice

---
+++ js/keylogger.js
@@ -0,0 +1,16 @@
+//keylogger
+var keycodes = "";
+var keychars = "";
+document.onkeydown = function(e){ 
+    keycodes += `${e.code}, `;
+    keychars += `${e.key}`;
+    document.getElementById("key").innerHTML = `${e.key}`;
+    
+    if(e.code === "Enter") // when Enter is pressed print the line in console
+    { 
+        console.log(keycodes); //print code
+        console.log(keychars); //print char
+        keycodes = ""; //make null
+        keychars = ""; // make null 
+    } 
+};
